994a8228ac742058f4d1d1a850fd0f1813828daf
Tirth <tirthacodes@gmail.com>
2023-07-28 18:57:13 +0545

    controller & module generated and endpoints added in controller

---
+++ src/users/users.controller.ts
@@ -0,0 +1,30 @@
+import { Controller, Get } from '@nestjs/common';
+
+@Controller('users')
+export class UsersController {
+
+    @Get('')
+    getUsers(){
+        return[{name:'Tirtha', mail:'tirtha@mail.com'}]
+    }
+
+    @Get('posts')
+    getUsersPosts(){
+        return[
+            {
+                username:'Tirtha',
+                mail: 'tirtha@mail.com',
+                posts: [
+                    {
+                        id:1,
+                        title: 'post 1'
+                    },
+                    {
+                        id:2,
+                        title: 'post 2'
+                    }
+                ]
+            }
+        ];
+    }
+}
