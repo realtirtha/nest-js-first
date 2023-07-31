ddbf42669251b741eeac62f6b26652f98a015690
Tirth <tirthacodes@gmail.com>
2023-07-31 20:40:34 +0545

    injecting dependency injection into controller

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,9 +1,14 @@
 import { Body, Controller, Get, Post, Req, Res, Param, Query, UsePipes, ValidationPipe, ParseIntPipe, ParseBoolPipe } from '@nestjs/common';
 import { Request, Response } from 'express';
 import { CreateUserDto } from './dtos/CreateUser.dto';
+import { UsersService } from './users.service';
 
 @Controller('users')
 export class UsersController {
+
+    constructor(private userService: UsersService){
+
+    }
 
     @Get()
     getUsers(@Query('sortDesc', ParseBoolPipe) sortDesc: boolean){
