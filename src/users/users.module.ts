26bd72dbe9819cbab64a6448ba7c7af359ea9265
Tirth <tirthacodes@gmail.com>
2023-08-02 19:01:37 +0545

    middle ware

--- src/users/users.module.ts
+++ src/users/users.module.ts
@@ -1,9 +1,14 @@
-import { Module } from '@nestjs/common';
+import { Module, NestModule, MiddlewareConsumer } from '@nestjs/common';
 import { UsersController } from './users.controller';
 import { UsersService } from './users.service';
+import { ExampleMiddleware } from 'src/example/example.middleware';
 
 @Module({
   controllers: [UsersController],
   providers: [UsersService]
 })
-export class UsersModule {}
+export class UsersModule implements NestModule {
+  configure(consumer: MiddlewareConsumer){
+    consumer.apply(ExampleMiddleware).forRoutes(UsersController);
+  }
+}
