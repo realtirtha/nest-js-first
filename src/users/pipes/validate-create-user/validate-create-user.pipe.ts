749953d69b105aa968a43c6022eb4f8696eebb1e
Tirth <tirthacodes@gmail.com>
2023-08-03 22:27:01 +0545

    i think i am doing pipes

---
+++ src/users/pipes/validate-create-user/validate-create-user.pipe.ts
@@ -0,0 +1,23 @@
+import { ArgumentMetadata, HttpException, Injectable, HttpStatus, PipeTransform } from '@nestjs/common';
+import { parse } from 'path';
+import { CreateUserDto } from 'src/users/dtos/CreateUser.dto';
+
+@Injectable()
+export class ValidateCreateUserPipe implements PipeTransform {
+  transform(value: CreateUserDto, metadata: ArgumentMetadata) {
+    console.log('Inside ValidateCreateUserPipe:');
+    console.log(value);
+    console.log(metadata);
+
+    const parseAgeToInt = parseInt(value.age.toString());
+    if(isNaN(parseAgeToInt)){
+      console.log('${value.age} is not a number!');
+      throw new HttpException('Invalid DataType for property age. Expected Number', HttpStatus.BAD_REQUEST);
+    }
+    
+
+    console.log(`${parseAgeToInt} is a number. Returning...`)
+    
+    return value;
+  }
+}
--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -2,6 +2,7 @@ import { Body, Controller, Get, Post, Req, Res, Param, Query, UsePipes, Validati
 import { Request, Response } from 'express';
 import { CreateUserDto } from './dtos/CreateUser.dto';
 import { UsersService } from './users.service';
+import { ValidateCreateUserPipe } from './pipes/validate-create-user/validate-create-user.pipe';
 
 @Controller('users')
 export class UsersController {
@@ -22,7 +23,7 @@ export class UsersController {
 
     @Post('create')
     @UsePipes(new ValidationPipe())
-    createUser(@Body() userData:CreateUserDto){
+    createUser(@Body(ValidateCreateUserPipe) userData:CreateUserDto){
         console.log(userData.age.toPrecision());
         return this.userService.createUser(userData);
     }
